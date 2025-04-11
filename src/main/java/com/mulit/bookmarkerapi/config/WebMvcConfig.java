868240f0448846237b3a59c7494c48d5848a43d0
sreevani8 <sreevanidumpala@gmail.com>
2025-04-11 10:04:26 +0530

    this is my 3rd commit

---
+++ src/main/java/com/mulit/bookmarkerapi/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.mulit.bookmarkerapi.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOriginPatterns("*");
+    }
+
+}
